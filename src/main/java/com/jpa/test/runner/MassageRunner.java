56521069e5826a50901629ac7899dce05288822b
91911 <91911@DESKTOP-KOSDOKQ>
2021-03-01 00:32:59 +0530

    WH 8520 first time file update

---
+++ src/main/java/com/jpa/test/runner/MassageRunner.java
@@ -0,0 +1,14 @@
+package com.jpa.test.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MassageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+ 
+		System.out.println("welcome to spring boot ");
+	}
+
+}
